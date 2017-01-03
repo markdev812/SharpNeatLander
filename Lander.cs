fc14228a498487f5ca8a62a388093f8800851b77
markdev812 <mahogland@gmail.com>
2017-01-02 18:05:24 -0600

    Add project files.

---
+++ Lander.cs
@@ -0,0 +1,34 @@
+﻿namespace SharpNeatLander
+{
+    class Lander
+    {
+
+        public double Altitude { get; private set; }
+        public double Velocity { get; private set; }
+        public double Fuel { get; private set; }
+        public double Thrust { get; set; }
+
+        private double _gravity;
+
+
+        public void Init(double startingHeight, double startingFuel, double gravity)
+        {
+            Altitude = startingHeight;
+            Fuel = startingFuel;
+            _gravity = gravity;
+        }
+        public void Update(double deltaTime)
+        {
+            if (Fuel <= 0)
+                Thrust = 0;
+            Fuel -= Thrust;
+            Velocity += (Thrust + _gravity) * deltaTime;
+
+            if (Velocity < Program.TerminalVel)
+                Velocity = Program.TerminalVel;
+
+            Altitude += Velocity * deltaTime;
+
+        }
+    }
+}
