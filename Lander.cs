b6fa985569d522e23f56dba13f2aeca9d65148c8
markdev812 <mahogland@gmail.com>
2017-01-08 23:17:48 -0600

    NormalizedFitness function

--- Lander.cs
+++ Lander.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using SharpNeat.Core;
-using SharpNeat.Phenomes;
+﻿using SharpNeat.Phenomes;
+using System;
 
 namespace SharpNeatLander
 {
     class Lander
     {
-		public const double StartingAltitude = 2400;
-		public const double StartingFuel = 500;
-		public const double Gravity = -3.711;
-		public const double TerminalVel = -200;
+        public const double StartingAltitude = 2400;
+        public const double StartingFuel = 200;
+        public const double Gravity = -3.711;
+        public const double TerminalVel = -200;
+        public const double CrashSpeed = -40;
 
-		public double Altitude { get; private set; }
+        public double Altitude { get; private set; }
         public double Velocity { get; private set; }
         public double Fuel { get; private set; }
         public double Thrust { get; set; }
@@ -29,82 +29,109 @@ namespace SharpNeatLander
         {
             if (Fuel <= 0)
                 Thrust = 0;
-            Fuel -= Thrust;
+            Fuel -= Thrust * deltaTime;
             Velocity += (Thrust + _gravity) * deltaTime;
 
             if (Velocity < TerminalVel)
                 Velocity = TerminalVel;
 
             Altitude += Velocity * deltaTime;
+            if (Altitude < 0)
+                Altitude = 0;
 
         }
-		public static double RunSimulation(IBlackBox box, bool playMode = false)
-		{
-			ISignalArray inputArr = box.InputSignalArray;
-			ISignalArray outputArr = box.OutputSignalArray;
+        public static double RunSimulation(IBlackBox box, bool playMode = false)
+        {
+            ISignalArray inputArr = box.InputSignalArray;
+            ISignalArray outputArr = box.OutputSignalArray;
 
-			//run the simulation
-			Lander ship = new Lander();
-			ship.Init(StartingAltitude, StartingFuel, Gravity);
+            //run the simulation
+            Lander ship = new Lander();
+            ship.Init(StartingAltitude, StartingFuel, Gravity);
 
-			for (int i = 1; i <= 100; i++)
-			{
-				//set inputs
-				inputArr[0] = ship.Altitude / StartingAltitude;
-				inputArr[1] = ship.Velocity / TerminalVel;
-				inputArr[2] = ship.Fuel / StartingFuel;
-				//inputArr[3] = (double)i / 100.0;
+            for (int i = 1; i <= 100; i++)
+            {
+                //set inputs
+                inputArr[0] = ship.Altitude / StartingAltitude;
+                inputArr[1] = ship.Velocity / TerminalVel;
+                inputArr[2] = ship.Fuel / StartingFuel;
+                //inputArr[3] = (double)i / 100.0;
 
-				box.Activate();
+                box.Activate();
 
-				ship.Thrust = Math.Round(outputArr[0] * 4); //Math.Floor(outputArr[0] * 4.0);
-															//Ship.Thrust = 3.42;
+                //if (outputArr[0] > 0.5)
+                //    ship.Thrust = 10;
+                ship.Thrust = Math.Round(outputArr[0] * 4); //Math.Floor(outputArr[0] * 4.0);
+                //Ship.Thrust = 3.42;
 
-				ship.Update(1);
-				if (playMode)
-					Console.WriteLine($"{i,-5}{ship.Altitude,8:F1}{ship.Velocity,8:F1}{ship.Fuel,8:F1}{ship.Thrust,8:F1}");
+                ship.Update(1);//0.25);
+                if (playMode)
+                    Console.WriteLine($"{i,-5}{ship.Altitude,8:F1}{ship.Velocity,8:F1}{ship.Fuel,8:F1}{ship.Thrust,8:F1}");
 
-				//did we hit the ground?
-				if (ship.Altitude <= 0)
-					break;
+                //did we hit the ground?
+                if (ship.Altitude <= 0)
+                {
+
+                    break;
+                }
 
 
-			}
+            }
 
-			return ship.CalcFitness();
-		}
+            return ship.CalcFitness();
+        }
 
-		public double CalcFitness()
-		{
-			//  fitness = fuel remaining
-			//  height > 1  didn't land  fitness -= HeightAboveSurface
-			//  height <= 1  landed
-			//      V < -40 m/s  on touchdown  fitness = 0 
-			//      V > -40  fitness += MaxVel - V   
+        double NormalizedFitness(double x, double origRangeA, double origRangeB, double desiredRangeA, double desiredRangeB)
+        {
+            //A,B = original range
+            //r1,r2 = desired range
+            //y = 1 + (x-A)*(r2-r1)/(B-A)
+            var fitness = 1 + (x - origRangeA) * (desiredRangeB - desiredRangeA) / (origRangeB - origRangeA);
+            return fitness;
+        }
 
-			double nFuel = Fuel / StartingFuel;
-			double nVelocity = Velocity / TerminalVel;
-			double nAltitude = Altitude / StartingAltitude;
 
-			double fitness = 0;
-			//small reward for lower velocity
-			fitness = (1 - nVelocity) * 5;
-			//fitness += (1 - nAltitude)* 10;
-			if (Altitude <= 2) //landed
-			{
-				if (Velocity > -40) //no crash
-				{
+        public double CalcFitness()
+        {
 
-					//big reward for safe landing
-					fitness += 100;
-					//fuel savings bonus
-					fitness += nFuel * 10;
 
-				}
+            double f = NormalizedFitness(Fuel, 1, StartingFuel, 1, 10);
+            double v = NormalizedFitness(Velocity, TerminalVel, CrashSpeed, 1, 10);
+            //double a = NormalizedFitness(Altitude, 1, StartingAltitude, 10, 1);
 
-			}
+            double fitness = f + v;
+            if (Velocity > CrashSpeed)
+            {
+                f = NormalizedFitness(Fuel, 1, StartingFuel, 1, 100);
+                v = NormalizedFitness(Velocity, CrashSpeed, -1, 1, 20);
+                fitness += f + v;
+                //fitness += 100;
+            }
+            //double nFuel = Fuel / StartingFuel;
+            //double nVelocity = Velocity / TerminalVel;
+            //double nAltitude = Altitude / StartingAltitude;
 
-			return fitness > 0 ? fitness : 0;
-		}
-	}
+            //double fitness = 0;
+            ////fitness = (1 - nAltitude);
+            //fitness += (CrashSpeed / Velocity) * 10;
+            ////fitness += nFuel * 5;
+            //if (Altitude <= 2) //landed
+            //{
+            //    //small reward for lower velocity
+            //    //fitness += (1 - nVelocity) * 5;
+            //    if (Velocity > CrashSpeed) //no crash
+            //    {
+
+            //        //big reward for safe landing
+            //        fitness += 100;
+            //        //fuel savings bonus
+            //        fitness += nFuel * 10;
+
+            //    }
+
+            //}
+            //Console.WriteLine($"{Altitude,8:F1}{Velocity,8:F1}{Fuel,8:F1}{Thrust,8:F1}");
+            return fitness > 0 ? fitness : 0;
+        }
+    }
 }
